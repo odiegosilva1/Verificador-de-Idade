ed783e3405dff792f1f2b874836802282573d0f4
Diego Silva <odiegosilvadev@gmail.com>
2023-08-24 10:17:59 -0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,19 @@
 function verificar() {
-    window.alert('cú')
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano =  document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')     
+    }else {
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)// Calcula idade.
+       var gênero = ''
+       if (fsex[0].checked) {
+        gênero = 'Homen'
+       } else if (fsex[1].checked){
+            gênero = 'Mulher'
+       }
+       res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    }
 }
